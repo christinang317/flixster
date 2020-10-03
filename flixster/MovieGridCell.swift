dd11d087b0d7537e0b93185a76d5324028f7e658
Christina Ng <christinasy317@gmail.com>
2020-10-03 13:09:44 -0400

    Pushing code from Week 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Christina Ng on 10/2/20.
+//  Copyright © 2020 Christina Ng. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIView!
+    
+    @IBOutlet weak var posterView2: UIImageView!
+}
